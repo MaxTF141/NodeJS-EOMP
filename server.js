bc1a4defa00d0fd04c3d17307d7e427fd7d0d4c0
MaxTF141 <isaacsmaxwill22@gmail.com>
2023-03-03 20:54:08 +0200

    final

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8080"
+  origin: "https://dead-mans-tales.web.app"
 };
 
 app.use(cors(corsOptions));
